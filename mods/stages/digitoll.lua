096a3098fe4fbd8e804dc85a4c03ea85755c248c
DPadderz <66304247+DPadderz@users.noreply.github.com>
2022-03-12 19:24:03 +0000

    Added icons.

--- mods/stages/digitoll.lua
+++ mods/stages/digitoll.lua
@@ -1,9 +1,12 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('modapps', 'modapps', 0, 0);
-	setScrollFactor('modapps', 0.9, 0.9);
+	makeLuaSprite('bgdigitoll', 'bgdigitoll', 0, 0);
+	setScrollFactor('bgdigitoll', 0.9, 0.9);
+	makeLuaSprite('frdigitoll', 'frdigitoll', 0, 0);
+	setScrollFactor('frdigitoll', 0.9, 0.9);
 
-	addLuaSprite('modapps', false);
+	addLuaSprite('bgdigitoll', false);
+	addLuaSprite('frdigitoll', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
