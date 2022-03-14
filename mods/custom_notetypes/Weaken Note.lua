afbc1b308f25f075948a412b47e69749c0a4bc0f
DPadderz <66304247+DPadderz@users.noreply.github.com>
2022-03-14 22:23:35 +0000

    Update Weaken Note.lua

--- mods/custom_notetypes/Weaken Note.lua
+++ mods/custom_notetypes/Weaken Note.lua
@@ -2,7 +2,7 @@ function onCreate()
 	--Iterate over all notes
 	for i = 0, getProperty('unspawnNotes.length')-1 do
 		--Check if the note is an Instakill Note
-		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'WeakenNote' then
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Weaken Note' then
 			setPropertyFromGroup('unspawnNotes', i, 'texture', 'weaken_note_assets'); --Change texture
 			setPropertyFromGroup('unspawnNotes', i, 'hitHealth', '-500'); --Default value is: 0.023, health gained on hit
 			setPropertyFromGroup('unspawnNotes', i, 'missHealth', '500'); --Default value is: 0.0475, health lost on miss
@@ -22,7 +22,7 @@ end
 -- noteType: The note type string/tag
 -- isSustainNote: If it's a hold note, can be either true or false
 function goodNoteHit(id, noteData, noteType, isSustainNote)
-	if noteType == 'WeakenNote' then
+	if noteType == 'Weaken Note' then
 		-- put something here if you want
 	end
 end
@@ -30,7 +30,7 @@ end
 -- Called after the note miss calculations
 -- Player missed a note by letting it go offscreen
 function noteMiss(id, noteData, noteType, isSustainNote)
-	if noteType == 'WeakenNote' then
+	if noteType == 'Weaken Note' then
 		-- put something here if you want
 	end
 end
