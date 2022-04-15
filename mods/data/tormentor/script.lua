341a58e5ccd68b1da9e1bbd58ece64d5f30ea492
AT <2ljams99@gmail.com>
2022-04-14 20:03:39 -0700

    Funny Floating script (+test notes event)

---
+++ mods/data/tormentor/script.lua
@@ -0,0 +1,25 @@
+local bfLosing = false
+local dadLosing = false
+
+function onUpdate(elapsed)
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+cameraShake(game, 0.005, 0.2)
+characterPlayAnim('gf', 'scared', true)
+doTweenZoom('camerazoom','camGame',0.8,0.26,'quadInOut')
+setProperty('health', getProperty('health') - 2 * ((getProperty('health')/22))/6)
+end
+function goodNoteHit(id, direction, noteType, isSustainNote)
+doTweenZoom('camerazoom','camGame',1.3,0.6,'quadInOut')
+end
+doTweenX('iconDadFadeEventTween', 'iconP2', 100, 0.001, 'linear');
+songPos = getSongPosition()
+		local currentBeat = (songPos/5000)*(curBpm/60)
+setCharacterY('dad',getCharacterY('dad') + (math.sin(currentBeat) * 0.2))
+function onMoveCamera(focus)
+	if focus == 'boyfriend' then
+		-- called when the camera focus on boyfriend
+	elseif focus == 'dad' then
+		setProperty('camFollowPos.y',getProperty('camFollowPos.y') + (math.sin(currentBeat) * 0.2))
+	end
+end
+end
