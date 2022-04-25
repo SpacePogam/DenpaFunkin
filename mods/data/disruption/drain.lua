1cc3cfbfa8959a80b096bd97369b9a0abb830fb6
AT <2ljams99@gmail.com>
2022-04-24 18:15:02 -0700

    0.1.3
    Read source commit notes

---
+++ mods/data/disruption/drain.lua
@@ -0,0 +1,5 @@
+function onUpdate(elapsed)
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+setProperty('health', getProperty('health') - 2 * ((getProperty('health')/22))/6)
+end
+end
