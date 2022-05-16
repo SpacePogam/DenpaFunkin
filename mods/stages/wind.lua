93c289471664f652d8e32a215b1a6c8747c9ba10
AT <2ljams99@gmail.com>
2022-05-15 21:38:20 -0700

    Better wind bg

--- mods/stages/wind.lua
+++ mods/stages/wind.lua
@@ -1,9 +1,39 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('bgwind', 'bgwind', 0, 0);
-	setScrollFactor('bgwind', 0.9, 0.9);
+	makeLuaSprite('bgwind01', 'bgwind01', -300, 0);
+	setScrollFactor('bgwind01', 0.1, 0.1);
 
-	addLuaSprite('bgwind', false);
+	addLuaSprite('bgwind01', false);
+
+	makeLuaSprite('bgwind02', 'bgwind02', -200, 50);
+	setScrollFactor('bgwind02', 1, 1);
+
+	addLuaSprite('bgwind02', false);
+
+	makeLuaSprite('bgwind05', 'bgwind04', -200, 50);
+	setScrollFactor('bgwind05', 1, 1);
+
+	addLuaSprite('bgwind05', false);
+
+	makeLuaSprite('bgwind03', 'bgwind03', -200, 50);
+	setScrollFactor('bgwind03', 1.1, 1);
+
+	addLuaSprite('bgwind03', false);
+
+	makeLuaSprite('bgwind04', 'bgwind05', -300, 220);
+	setScrollFactor('bgwind04', 1.4, 1);
+
+	addLuaSprite('bgwind04', true);
+
+	makeLuaSprite('bgwind06', 'bgwind06', 150, 220);
+	setScrollFactor('bgwind06', 1.4, 1);
+
+	addLuaSprite('bgwind06', true);
+
+	makeLuaSprite('bgwind07', 'bgwind07', -100, 400);
+	setScrollFactor('bgwind07', 1.3, 1);
+
+	addLuaSprite('bgwind07', true);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
